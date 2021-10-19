9e6ce666e2c7954ef1a4df1bdcc33947d8d64696
Nem-Ajvaz <45509371+Nem-Ajvaz@users.noreply.github.com>
2021-10-19 23:39:49 +1100

    Updates

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-import "./index.css";
-
-const App = () => <PortfolioContainer />;
-
-export default App;
